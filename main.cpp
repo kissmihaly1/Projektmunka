98415fc860925d9cf97d0cdf9c869bc1951727f8
kissmihaly1 <124740303+kissmihaly1@users.noreply.github.com>
2023-10-19 17:59:47 +0200

    Update negative

--- main.cpp
+++ main.cpp
@@ -2,46 +2,50 @@
 #include <iostream>
 using namespace std;
 
+
+
+
 // A negatív szám kezelésekor dönti el, hogy melyik oldalra kell csökkenteni az intervallumot.
 
-int handleNegativeNumber(multimap<int, int> &intervals, multimap<int, int>::iterator it0, int number, int amount) {
-    bool validnumber=true;
+void handleNegativeNumber(multimap<int, int> &intervals, multimap<int, int>::iterator it0, int num) {
+    bool validNumber=true;
     int whichSide;
     auto it1=it0;
     ++it1;
-    if (number>it1->first || number<it0->first){
-        validnumber=false;
-        return amount;
+    if (num>it1->first || num<it0->first){
+        validNumber=false;
     }
-        if (it1 != intervals.end() && it0->first < number && it1->first > number) {
-            intervals.erase(number);
-            intervals.insert(make_pair(number - 1, 0));
-            intervals.insert(make_pair(number + 1, 1));
+    if(validNumber) {
+        if (it1 != intervals.end() && it0->first < num && it1->first > num) {
+            intervals.erase(num);
+            intervals.insert(make_pair(num - 1, 0));
+            intervals.insert(make_pair(num + 1, 1));
             whichSide = 2;
-        } else if (it0->first < number) {
+        } else if (it0->first < num) {
             whichSide = 1;
         } else {
             whichSide = 0;
         }
 
         if (whichSide == 1) {
-            intervals.insert(make_pair(number - 1, whichSide));
-            intervals.erase(number);
+            intervals.insert(make_pair(num - 1, whichSide));
+            intervals.erase(num);
         } else if (whichSide == 0) {
-            intervals.insert(make_pair(number + 1, whichSide));
-            intervals.erase(number);
+            intervals.insert(make_pair(num + 1, whichSide));
+            intervals.erase(num);
         }
-        return amount-1;
     }
 
+}
 // Módosítja az intervallumot a megadott helyen.
 
-void modifyInterval(multimap<int, int> &intervals, multimap<int, int>::iterator it0, int number, int whichSide) {
-        intervals.insert(make_pair(number, whichSide));
-        intervals.erase(it0);
+void modifyInterval(multimap<int, int> &intervals, multimap<int, int>::iterator it0, int num, int whichSide) {
+    intervals.insert(make_pair(num, whichSide));
+    intervals.erase(it0);
 }
 
 // Kiírja az intervallumokat.
+
 
 void printResult(multimap<int, int> &intervals){
     auto it = intervals.begin();
@@ -52,75 +56,57 @@ void printResult(multimap<int, int> &intervals){
         it++;
     }
 }
+void addNumber(multimap<int, int> &intervals, int num, int amount) {
+    auto it = intervals.find(num);
+    auto it0 = intervals.lower_bound(num);
+    auto it1 = it0;
+    auto it2 = it0;
+    if (it != intervals.end()) {
+        intervals.insert(make_pair(num, it->second));
+        intervals.erase(it);
+    }
+    else {
+        if (it0 != intervals.begin()) {
+            it0--;
+            if (num - it0->first == 1) {
+                modifyInterval(intervals, it0, num, 1);
+                amount+=1;
+                return;
+            }
+        }
+        auto it3 = it0;
+        if (it3 != intervals.end() && it3->first - num == 1) {
+            modifyInterval(intervals, it3, num, 0);
+            return;
+        }
 
-//main
+        intervals.insert(make_pair(num, 0));
+        intervals.insert(make_pair(num, 1));
+    }
+}
+
 
 int main(){
+    int amount=1;
     multimap<int, int> intervals;
-    int amount=0;
     int num = -1;
     intervals.clear();
     while (num != 0) {
         cout << "Enter a number: ";
         cin >> num;
         if (num != 0) {
-            if (num < 0) {
+            if (num < 0&&amount>1) {
                 auto it2 = intervals.lower_bound(num);
                 num = abs(num);
-                handleNegativeNumber(intervals,it2, num, amount);
+                handleNegativeNumber(intervals, it2, num);
+                amount--;
             }
             else {
-                if (intervals.empty()) {
-                    intervals.insert(make_pair(num, 0));
-                    intervals.insert(make_pair(num, 1));
-                    amount++;
-                }
-                else {
-                    auto it = intervals.find(num);
-                    if (it == intervals.end()) {
-                        auto it0 = intervals.lower_bound(num);
-                        auto it1 = intervals.lower_bound(num);
-
-                        if (it0 == intervals.begin()) {
-                            if (it0->first - num == 1) {
-                                modifyInterval(intervals,it0, num, 0 );
-                            } else {
-                                intervals.insert(make_pair(num, 0));
-                                intervals.insert(make_pair(num, 1));
-                            }
-                        } else if (it0 == intervals.end()) {
-                            it0--;
-                            if (num - it0->first == 1) {
-                                modifyInterval(intervals,it0, num, 1);
-                            } else {
-                                intervals.insert(make_pair(num, 0));
-                                intervals.insert(make_pair(num, 1));
-                            }
-                        } else {
-                            if (it0->second == 0) {
-                                it0--;
-                                if ((num - it0->first == 1) || (it1->first - num == 1)) {
-                                    if ((num - it0->first == 1) && (it1->first - num == 1)) {
-                                        intervals.erase(it0);
-                                        intervals.erase(it1);
-                                    } else if (num - it0->first == 1) {
-                                        modifyInterval(intervals,it0, num, 1);
-                                    } else {
-                                        modifyInterval(intervals,it1, num, 0);
-                                    }
-                                } else {
-                                    intervals.insert(make_pair(num, 0));
-                                    intervals.insert(make_pair(num, 1));
-                                }
-                            }
-                        }
-                    }
-
-                }
+                addNumber(intervals, num, amount);
                 amount++;
             }
         }
-        if (amount>=1) {
+        if (amount>1) {
             printResult(intervals);
         }
         else{
