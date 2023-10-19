d1aab2be72056f6a3babec8629d08be5ae527262
kissmihaly1 <124740303+kissmihaly1@users.noreply.github.com>
2023-10-19 19:14:33 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,116 +2,131 @@
 #include <iostream>
 using namespace std;
 
-
-
-
 // A negatív szám kezelésekor dönti el, hogy melyik oldalra kell csökkenteni az intervallumot.
 
-void handleNegativeNumber(multimap<int, int> &intervals, multimap<int, int>::iterator it0, int num) {
-    bool validNumber=true;
-    int whichSide;
-    auto it1=it0;
-    ++it1;
-    if (num>it1->first || num<it0->first){
-        validNumber=false;
-    }
-    if(validNumber) {
-        if (it1 != intervals.end() && it0->first < num && it1->first > num) {
-            intervals.erase(num);
-            intervals.insert(make_pair(num - 1, 0));
-            intervals.insert(make_pair(num + 1, 1));
-            whichSide = 2;
-        } else if (it0->first < num) {
-            whichSide = 1;
-        } else {
-            whichSide = 0;
-        }
+void handleNegativeNumber(multimap<int, int> &intervals, int num) {
 
-        if (whichSide == 1) {
-            intervals.insert(make_pair(num - 1, whichSide));
-            intervals.erase(num);
-        } else if (whichSide == 0) {
-            intervals.insert(make_pair(num + 1, whichSide));
-            intervals.erase(num);
+    auto it0 = intervals.find(num);
+    auto it1 = intervals.find(num);
+    if (it0==intervals.end()){
+    }
+    else{
+            ++it1;
+
+        if (it1->first==it0->first){  //ha ugyanaz
+            intervals.erase(it0);
+            intervals.erase(it1);
+            return;
+        }
+        if (it0->first==num){
+            if (it0->second==0){
+                intervals.insert(make_pair(num+1, 0)); //also vegpont novelese
+                intervals.erase(it0);
+                return;
+            }
+            else{
+                intervals.insert(make_pair(num-1, 1)); //felso vegpont csokkentese
+                intervals.erase(it0);
+                return;
+            }
+        }
+    }
+    if (it0==intervals.end()){
+        auto it3 = intervals.lower_bound(num);
+        auto it2 = intervals.lower_bound(num);
+        --it2;
+        if (it2->first < num && it3->first > num) {
+            intervals.insert(make_pair(num-1, 0));
+            intervals.insert(make_pair(num+1, 1));
         }
     }
 
 }
 // Módosítja az intervallumot a megadott helyen.
 
-void modifyInterval(multimap<int, int> &intervals, multimap<int, int>::iterator it0, int num, int whichSide) {
+void modifyInterval(multimap<int, int> &intervals, multimap<int, int>::iterator it0, int num, int whichSide){
     intervals.insert(make_pair(num, whichSide));
     intervals.erase(it0);
 }
+// Hozzáad egy új intervallumot.
 
+void addInterval(multimap<int, int> &intervals, int num){
+    intervals.insert(make_pair(num, 0));
+    intervals.insert(make_pair(num, 1));
+}
 // Kiírja az intervallumokat.
 
 
 void printResult(multimap<int, int> &intervals){
-    auto it = intervals.begin();
-    while (it != intervals.end()){
-        cout << "["<< it->first << ", ";
-        it++;
-        if (it != intervals.end()) cout << it->first << "]" << endl;
-        it++;
+    if (!intervals.empty()) {
+        auto it = intervals.begin();
+        while (it != intervals.end()) {
+            cout << "[" << it->first << ", ";
+            it++;
+            if (it != intervals.end()) cout << it->first << "]" << endl;
+            it++;
+        }
+    }
+    else{
+        cout<< "[]" <<endl;
     }
 }
-void addNumber(multimap<int, int> &intervals, int num, int amount) {
-    auto it = intervals.find(num);
-    auto it0 = intervals.lower_bound(num);
-    auto it1 = it0;
-    auto it2 = it0;
-    if (it != intervals.end()) {
-        intervals.insert(make_pair(num, it->second));
-        intervals.erase(it);
-    }
-    else {
-        if (it0 != intervals.begin()) {
-            it0--;
-            if (num - it0->first == 1) {
-                modifyInterval(intervals, it0, num, 1);
-                amount+=1;
-                return;
-            }
-        }
-        auto it3 = it0;
-        if (it3 != intervals.end() && it3->first - num == 1) {
-            modifyInterval(intervals, it3, num, 0);
-            return;
-        }
-
-        intervals.insert(make_pair(num, 0));
-        intervals.insert(make_pair(num, 1));
-    }
-}
-
 
 int main(){
-    int amount=1;
     multimap<int, int> intervals;
     int num = -1;
-    intervals.clear();
     while (num != 0) {
         cout << "Enter a number: ";
         cin >> num;
         if (num != 0) {
-            if (num < 0&&amount>1) {
-                auto it2 = intervals.lower_bound(num);
+            if (num < 0) {
                 num = abs(num);
-                handleNegativeNumber(intervals, it2, num);
-                amount--;
+                handleNegativeNumber(intervals, num);
             }
             else {
-                addNumber(intervals, num, amount);
-                amount++;
+                if (intervals.empty()) {
+                    addInterval(intervals, num);
+                } else {
+                    auto it = intervals.find(num);
+                    if (it == intervals.end()) {
+                        auto it0 = intervals.lower_bound(num);
+                        auto it1 = intervals.lower_bound(num);
+
+                        if (it0 == intervals.begin()) {
+                            if (it0->first - num == 1) {
+                                modifyInterval(intervals,it0, num, 0 );
+                            } else {
+                                addInterval(intervals, num);
+                            }
+                        } else if (it0 == intervals.end()) {
+                            it0--;
+                            if (num - it0->first == 1) {
+                                modifyInterval(intervals,it0, num, 1);
+                            } else {
+                                addInterval(intervals, num);
+                            }
+                        } else {
+                            if (it0->second == 0) {
+                                it0--;
+                                if ((num - it0->first == 1) || (it1->first - num == 1)) {
+                                    if ((num - it0->first == 1) && (it1->first - num == 1)) {
+                                        intervals.erase(it0);
+                                        intervals.erase(it1);
+                                    } else if (num - it0->first == 1) {
+                                        modifyInterval(intervals,it0, num, 1);
+                                    } else {
+                                        modifyInterval(intervals,it1, num, 0);
+                                    }
+                                } else {
+                                    addInterval(intervals, num);
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-        if (amount>1) {
-            printResult(intervals);
-        }
-        else{
-            cout<< "[]"<<endl;
-        }
+        printResult(intervals);
     }
     return 0;
 }
